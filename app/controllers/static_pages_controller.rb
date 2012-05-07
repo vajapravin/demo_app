86192d7f896cb148bb0b78f1cdc9d66b76948f18
vajapravin <vajapravin23@gmail.com>
2012-05-07 14:19:20 +0530

    Filling layout

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,14 +4,10 @@ class StaticPagesController < ApplicationController
 
   def help
   end
-<<<<<<< HEAD
-  
+
   def contact    
   end
-  
-=======
 
->>>>>>> 8ce5aab3e00c220001ae701cb6441f1f358e78f8
   def about
   end
 end
