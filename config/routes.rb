6a523a2de22079c95d66855ed3e5d09b3e59c3bc
vajapravin <vajapravin23@gmail.com>
2012-05-08 20:26:11 +0530

    chapter7 is continued...

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 DemoApp::Application.routes.draw do
+  get "static_pages/home"
+
+  get "static_pages/help"
+
+  get "users/new"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
