b660136aac7fd9322fad9bcf66608c07dda6f400
vajapravin <vajapravin23@gmail.com>
2012-05-11 17:33:10 +0530

    chapter 8 is done

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -6,6 +6,14 @@ describe "AuthenticationPages" do
   describe "signin page" do
     before { visit signin_path }
 
+  
+    describe "with invalid information" do
+      before { click_button "Sign in" }
+
+      it { should have_selector('title', text: 'Sign in') }
+      it { should have_selector('div.alert.alert-error', text: 'Invalid') }
+    end
+    
     describe "with valid information" do
       let(:user) { FactoryGirl.create(:user) }
       before do
@@ -18,6 +26,11 @@ describe "AuthenticationPages" do
       it { should have_link('Profile', href: user_path(user)) }
       it { should have_link('Sign out', href: signout_path) }
       it { should_not have_link('Sign in', href: signin_path) }
+      
+      describe "followed by signout" do
+        before { click_link "Sign out" }
+        it { should have_link('Sign in') }
+      end
     end
   end
 end
